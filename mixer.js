1404efa11579e3c398a39fb5e50c4aabdd20a65c
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2018-01-21 02:47:31 +0900

    accessor 以外作成

--- mixer.js
+++ mixer.js
@@ -42,12 +42,89 @@ var mixer = {
     return this.extend(target, this.use(name));
   },
 
+  get: function(name) {
+    return this._objects[name];
+  },
+
+  getAccessor: function(name, circulator) {
+    circulator = circulator || {};
+    if (circulator[name]) return {};
+  },
+
   use: function(name, circulator) {
 
+    circulator = circulator || {};
+    if (circulator[name]) return {};
+    var p = mixer.extend({}, mixer._objects[name] || {});
+    var accessor = mixer.extend({}, p.accessor);
+    var target = {};
+    if (p.supers) {
+      p.supers.forEach(function(parent) {
+        mixer.extend(target, mixer.use(parent, circulator));
+        // mixer.extend(p.accessor, mixer.getAccessor(parent, circulator));
+        circulator[parent] = true;
+      });
+    }
+    mixer.extend(target, p.props);
+    mixer.defineAccessor(target, accessor);
+    return target;
+
   },
 
-  add: function() {
+  add: function(name, supers, props, descMap) {
+    if (typeof supers === 'undefined') {
+      descMap = props;
+      props = supers;
+      supers = null;
+    }
+    if (typeof supers === 'string') {
+      supers = [supers];
+    }
+    this._objects[name] = {
+      __mixerName: name,
+      supers: supers,
+      props: props || {},
+      accessor: descMap || {},
+    };
 
+    return this.wrapDefer(name);
   },
 
+  accessor: function(target, descMap) {
+    for (var k in descMap) {
+      var desc = descMap[k];
+      var d = {
+        enumerable: true,
+        configurable: true,
+      };
+
+      if (desc.get) d.get = desc.get;
+      if (desc.set) d.set = desc.set;
+      Object.defineProperty(target, k, d);
+    }
+    return target;
+  },
+
+  defineAccessor: function(name, descMap) {
+    var def = this.get(name);
+    for (var k in descMap) {
+      def.accessor[k] = descMap[k];
+    }
+    return this.wrapDefer(name);
+  },
+
+  wrapDefer: function(name) {
+    return mixer.extend({ name: name }, mixer._deferProperty);
+  },
+
+  _deferProperty: {
+    accessor: function(descMap) {
+      return mixer.defineAccessor(this.name, descMap);
+    },
+
+    extend: function(property) {
+      mixer.extend(mixer.get(this.name).props, property);
+      return this;
+    }
+  }
 };
